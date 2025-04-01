36fcd5f939a8c4b70acd48c25f6b6f9d74d4011e
luis112002 <148033715+luis112002@users.noreply.github.com>
2025-04-01 11:10:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-from fastapi import FastAPI, UploadFile, File
-import boto3
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
 import os
 
-app = FastAPI()
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY")
-)
-
-BUCKET_NAME = os.environ.get("BUCKET_NAME")
-
-@app.get("/")
-def read_root():
-    return {"mensaje": "Bienvenido a la API de FastAPI con App Runner y S3"}
-
-@app.post("/subir-archivo")
-def subir_archivo(archivo: UploadFile = File(...)):
-    s3.upload_fileobj(archivo.file, BUCKET_NAME, archivo.filename)
-    return {"mensaje": f"Archivo '{archivo.filename}' subido correctamente a S3"}
-
-@app.get("/listar-archivos")
-def listar_archivos():
-    objetos = s3.list_objects_v2(Bucket=BUCKET_NAME)
-    archivos = [obj['Key'] for obj in objetos.get('Contents', [])]
-    return {"archivos": archivos}
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT"))
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
